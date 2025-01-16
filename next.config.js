def8e87cb915ca3b231e98a2c563fcf0632ff68d
GerardoFernandez7 <gerardofernandezcruzmt@gmail.com>
2025-01-15 22:42:41 -0600

    deploy

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/PlayRadar',
+  assetPrefix: '/PlayRadar/',
+}
+
+module.exports = nextConfig 
