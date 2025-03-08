dc2a6bbc8632cb22dc090268e0ae0f39056ab7e2
GerardoFernandez7 <gerardofernandezcruzmt@gmail.com>
2025-03-07 22:51:18 -0600

    fix: :bug: beating Git Hub

---
+++ playradar/app/components/features/ThemeSelector.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "../ui/Button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/ThemeDropdown";
+
+export default function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="border-gray-500 dark:border-gray-300 bg-transparent shadow-none hover:bg-transparent  hover:scale-110"
+        >
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 fill-gray-800 dark:stroke-white dark:fill-black" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
---
+++ playradar/app/components/ui/Loader.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+interface LoadingAnimationProps {
+  className?: string;
+  size?: number;
+}
+
+const LoadingAnimation: React.FC<LoadingAnimationProps> = ({
+  className = "",
+  size = 50,
+}) => {
+  return (
+    <div className={`flex justify-center items-center ${className}`}>
+      <div className={`size-${size}`}>
+        <DotLottieReact
+          src="https://lottie.host/7b4dd0bd-fedc-41a6-b542-8d7c1950999a/3fmNE4stxF.lottie"
+          loop
+          autoplay
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoadingAnimation;
