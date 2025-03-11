17a7dd4fc61b7399faf6565adf87e7d7bd0e07df
GerardoFernandez7 <gerardofernandezcruzmt@gmail.com>
2025-03-11 17:46:48 -0600

    fix: :ambulance: fix _not-found error

---
+++ playradar/app/_not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+      <div className="grid h-screen place-items-center">
+        <h1 className="text-4xl font-bold">404 - PlayRadar no encontró esta página</h1>
+      </div>
+    );
+  }
